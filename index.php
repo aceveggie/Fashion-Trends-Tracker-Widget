576c3a9874fa230dc68c16a2c0724c1355a2080d
Rahul Kavi <leorahul16@gmail.com>
2013-12-24 07:01:47 -0500

    fixed class names

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 if ( ! defined('WP_CONTENT_DIR')) exit('No direct script access allowed');
 /*------------------------------------------------------------------------------
-Plugin Name: Fashion Trends Tracker Widget
+Plugin Name: Fashion Trends Tracker
 Plugin URI: http://www.rahulkavi.com/
 Description: Widget to track fashion trends in your blog
 Author: Rahul Kavi
@@ -9,22 +9,19 @@ Version: 0.1
 Author URI: http://www.rahulkavi.com/
 ------------------------------------------------------------------------------*/
 // include() or require() any necessary files here...
-include_once('includes/ContentRotator.php');
-include_once('includes/ContentRotatorWidget.php');
+include_once('includes/FashionTrendTracker.php');
+include_once('includes/FashionTrendTrackerWidget.php');
 
 
 // Tie into WordPress Hooks and any functions that should run on load.
-add_action('widgets_init', 'ContentRotatorWidget::register_this_widget');
+add_action('widgets_init', 'FashionTrendTrackerWidget::register_this_widget');
 
-//add_action('admin_menu', 'ContentRotator::add_menu_item');
-// add_action( 'wp_enqueue_scripts', 'ContentRotator::include_custom_ajax_call_scripts' );
-// add_action( 'admin_init', 'ContentRotator::add_js_to_admin_page' );
+//add_action('admin_menu', 'FashionTrendTracker::add_menu_item');
+// add_action( 'wp_enqueue_scripts', 'FashionTrendTracker::include_custom_ajax_call_scripts' );
+// add_action( 'admin_init', 'FashionTrendTracker::add_js_to_admin_page' );
 
 // load a post page load jquery ui css
-add_action('widgets_init', 'ContentRotator::add_js_css_to_post_page');
-// add_action('widgets_init', 'ContentRotator::include_latest_accordion_css_js');
-
-
-
+add_action('widgets_init', 'FashionTrendTracker::add_js_css_to_post_page');
+// add_action('widgets_init', 'FashionTrendTracker::include_latest_accordion_css_js');
 
 /* EOF */
